2cc5cc2b506bbdc1cebbe2554022ecbe73087df8
Tyuiu.SmirnovMN@outlook.com <Tyuiu.SmirnovMN@outlook.com>
2023-10-06 23:50:59 +0500

    Task3

---
+++ Tyuiu.SmirnovMN.Sprint2.Task3.V14.Test/DataServiceTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SmirnovMN.Sprint2.Task3.V14.Lib;
+
+
+namespace Tyuiu.SmirnovMN.Sprint2.Task3.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = -25;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.75;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -5;
+            double res = ds.Calculate(x);
+            double wait = 0.822;
+            Assert.AreEqual(wait, res);
+
+        }
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -20;
+            double res = ds.Calculate(x);
+            double wait = -219.95;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
+}
---
+++ Tyuiu.SmirnovMN.Sprint2.Task3.V14/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SmirnovMN.Sprint2.Task3.V14.Lib;
+
+namespace Tyuiu.SmirnovMN.Sprint2.Task3.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Смирнов М.Н | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Смирнов Михаил Николаевич | ИИПб-23-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *");
+            Console.WriteLine("* с использованием вложенных оператор if-else, где пользователь вводит    *");
+            Console.WriteLine("* значение переменной X с клавиатуры                                      *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
